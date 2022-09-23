2295b53b41a4a0e57514600bc9fdd56a5233d4c7
TOM <chinaid@msn.com>
2022-09-23 14:22:51 +0800

    Update squid_log_to_readable.cpp

--- squid_log_to_readable.cpp
+++ squid_log_to_readable.cpp
@@ -20,29 +20,28 @@ string convert_unixtimestamp_to_readable(string text, string converted_file) {
 	ss << put_time(std::gmtime(&unix_timestamp), "%Y-%m-%d %I:%M:%S %p");
 	content[0] = ss.str();
 	string result;
-	for (auto item: content){
+	for (auto item: content) {
 		 result += item + delimiter;
 	}	
 	return result;
 }
 
-int main(int argc, char *argv[])
-{
-	if (argc < 2){
+int main(int argc, char *argv[]){
+	if (argc < 2) {
 		cout << "Error: 2 arguments are required, only " << argc << " provied" << endl;
 		cout << "Usage: squid_log_to_readable <orginal_log_file> <converted_log_file>" << endl; 
 	}
-  ifstream infile(argv[1]);
-  ofstream outfile(argv[2], ios::out);
+        ifstream infile(argv[1]);
+        ofstream outfile(argv[2], ios::out);
 	if(infile.is_open() && outfile.is_open()) {
-    string line;
-    while (getline(infile, line)) {
-      outfile << convert_unixtimestamp_to_readable(line, argv[2]) << endl;
+        string line;
+        while (getline(infile, line)) {
+            outfile << convert_unixtimestamp_to_readable(line, argv[2]) << endl;
+        }
     }
-  }
 	outfile.close();
-  infile.close();
-  cout << "Info: Orginal log file: " << argv[1] << endl;
-  cout << "Info: coverted log file: " << argv[2] << endl;
+    infile.close();
+    cout << "Info: Orginal log file: " << argv[1] << endl;
+    cout << "Info: Coverted log file: " << argv[2] << endl;
 	return EXIT_SUCCESS;
 }
